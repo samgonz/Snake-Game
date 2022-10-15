7045957a25cf1a127d3df6570a93ad2dad6bb667
samgonz <doomteam1@gmail.com>
2022-10-14 21:44:21 -0500

    Added the snake body to follow the snake head.

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ screen.tracer(0)
 starting_positions = [(0.00, 0.00), (-20.00, 0.00), (-40.00, 0.00)]
 segments = []
 
-for position in starting_positions:
+# Creates the snake segments shapes and colors. 
+for position in starting_positions: 
     new_snake_body = Turtle()
     new_snake_body.color(224, 234, 255)
     new_snake_body.penup()
@@ -20,11 +21,16 @@ for position in starting_positions:
     segments.append(new_snake_body)
 
 game_is_on = True
-
+# Creates a loop that updates the screen every .1 second.
 while game_is_on:
     screen.update()
     time.sleep(.1)
     
-    for seg_num in range()
+    # Sets body of the snake to follow the head of the snake.
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)    
         
 screen.exitonclick()
