b738e7c12a7ac9c45cc147351aa5bb5b500b42bf
samgonz <doomteam1@gmail.com>
2022-10-16 22:19:43 -0500

    added a food class item

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen
+from food import Food
 from snake import Snake
 
 screen = Screen()
@@ -12,10 +13,18 @@ screen.tracer(0)
 # Creates the snake segments shapes and colors. 
 snake = Snake()
 
+# Creates the food segments shapes and colors
+food = Food()
+
+# Creates a listener to listen for keyboard events
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
 game_is_on = True
 # Creates a loop that updates the screen every .1 second.
-
-
 while game_is_on:
     screen.update()
     time.sleep(.1)
