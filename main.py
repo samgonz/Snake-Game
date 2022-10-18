34224d1c91132a749f00a51eb19a199757e51b65
samgonz <doomteam1@gmail.com>
2022-10-17 22:53:56 -0500

    added in edge collision and body collision

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 from food import Food
+from gameover import game_over
+from scoreboard import Scoreboard
 from snake import Snake
 
 screen = Screen()
@@ -16,6 +18,9 @@ snake = Snake()
 # Creates the food segments shapes and colors
 food = Food()
 
+# Creates the scoreboard 
+scoreboard = Scoreboard()
+
 # Creates a listener to listen for keyboard events
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -30,4 +35,21 @@ while game_is_on:
     time.sleep(.1)
     snake.move()    
         
+    # Detect collision with food segments
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+    
+    # Detect collision with screen boundaries    
+    if snake.head.xcor() < -280 or snake.head.xcor() > 280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        game_is_on = False
+        
+    # Detect collision with snake body
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            
+end_game = game_over()
+
 screen.exitonclick()
